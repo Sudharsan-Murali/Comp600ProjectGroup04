a1d3ea48d75b35d57e3c6e51fa650200950dd297
janaki Katharotiya <jankikathrotiya19@gmail.com>
2025-04-08 16:05:11 -0600

    sql file changes - janki

---
+++ jobportal/src/Resources/sqlFiles/Job_Type.sql
@@ -0,0 +1,7 @@
+USE job_portal;
+
+CREATE TABLE IF NOT EXISTS Job_Type (
+    JobType_ID INT PRIMARY KEY NOT NULL,
+    Job_Type VARCHAR(50)
+);
+
---
+++ jobportal/src/Resources/sqlFiles/Security_Ques.sql
@@ -0,0 +1,7 @@
+USE job_portal;
+
+CREATE TABLE IF NOT EXISTS Security_Ques (
+    Que_ID INT PRIMARY KEY NOT NULL,
+    Security_Question VARCHAR(200)
+);
+
